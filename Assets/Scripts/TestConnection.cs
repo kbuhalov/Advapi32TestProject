a8f0595346db0194abf111b4520cfa82dec8e56c
kirill.buhalov <k.buhalov@heyworks.com>
2020-01-13 14:26:47 +0300

    create socket at start of the app

---
+++ Assets/Scripts/TestConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TestConnection : MonoBehaviour
+{
+    private void Start()
+    {
+        Connect();
+    }
+
+    private async void Connect()
+    {
+        var host = new Uri("wss://echo.websocket.org");
+
+        Debug.Log($"!!!!!Create new client web socket");
+
+        var s = new ClientWebSocket();
+
+        Debug.Log($"!!!!!Socket created");
+
+        Debug.Log($"!!!!!Establishing Connection to {host}");
+
+        await s.ConnectAsync(host, CancellationToken.None);
+
+        Debug.Log($"!!!!!Connection established with state: {s.State}");
+    }
+}
